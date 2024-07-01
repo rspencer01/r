57964ab0616e896497f8da20b92a52b18570661e
Robert Spencer <me@robertandrewspencer.com>
2024-07-01 18:58:53 +0100

    Make more rusty and bring under 100 lines
    This critical piece of software deserves to be well crafted.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 extern crate getrandom;
 
-use std::env;
-use std::fmt;
-use std::ops::Range;
+use std::{env, fmt};
 
-const USAGE_STRING : &str =
-"
-Usage:
+const USAGE_STRING: &str = "Usage:
   r <dice>
 
 Valid Dice:
@@ -17,67 +13,52 @@ Valid Dice:
   which indicates one should roll X dice with Y sides and add or subtract Z
   from the result.
 
-Exmple:
+Example:
   r 2d8+4
 ";
 
+#[derive(Debug)]
+struct InvalidDie;
+impl fmt::Display for InvalidDie {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Invalid die")
+    }
+}
+impl std::error::Error for InvalidDie {}
+
 #[derive(Copy, Clone)]
-struct DieRoll {
+struct DiceRoll {
     count: u16,
     sides: u16,
     modify: i16,
 }
 
-fn random_in(within : Range<u16>) -> u16 {
-    let dx = within.end - within.start;
-    let mx = (0xff_ff / dx) * dx;
-    let mut r : u16;
+fn random(upper_bound: u16) -> u16 {
+    let mx = (u16::MAX - upper_bound).next_multiple_of(upper_bound);
+    let mut r: u16;
+    let mut x = [0; 2];
     loop {
-        let mut x = [0; 2];
         getrandom::getrandom(&mut x).expect("Error obtaining random bits");
-        r = (x[0] as u16) << 8 | x[1] as u16;
+        r = u16::from_be_bytes(x);
         if r < mx {
             break;
         }
     }
-    within.start + r % dx
+    r % upper_bound
 }
 
-impl DieRoll {
-    fn from(desc : &str) -> Result<DieRoll, &str> {
-        let count;
-        let sides;
-        let modify;
-        let die;
-        match desc.find(|c| c == '+' || c == '-') {
-            Some(i) => {
-                modify = match desc[i..].parse() {
-                    Ok(x) => x,
-                    Err(_) => return Err("Not a valid die"),
-                };
-                die = &desc[..i];
-            }
-            None => {
-                modify = 0;
-                die = desc;
-            }
+impl DiceRoll {
+    fn from(desc: &str) -> Result<DiceRoll, InvalidDie> {
+        let mut modify = 0;
+        let mut die = desc;
+        if let Some(m_idx) = desc.find(|c| c == '+' || c == '-') {
+            modify = desc[m_idx..].parse().map_err(|_| InvalidDie)?;
+            die = &desc[..m_idx];
         };
-        match die.find('d') {
-            Some(i) => {
-                count = match die[..i].parse::<u16>() {
-                    Ok(x) => x,
-                    Err(_) => return Err("Not a valid die"),
-                };
-                sides = match die[i+1..].parse::<u16>() {
-                    Ok(x) => x,
-                    Err(_) => return Err("Not a valid die"),
-                };
-            }
-            None => {
-                return Err("Not a valid die");
-            }
-        }
-        Ok(DieRoll {
+        let d_index = die.find('d').ok_or(InvalidDie)?;
+        let count = die[..d_index].parse().map_err(|_| InvalidDie)?;
+        let sides = die[d_index + 1..].parse().map_err(|_| InvalidDie)?;
+        Ok(DiceRoll {
             count,
             sides,
             modify,
@@ -85,16 +66,17 @@ impl DieRoll {
     }
 
     fn roll(&self) -> i16 {
-        (0..self.count).map(|_| {random_in(1..self.sides+1)}).sum::<u16>() as i16 + self.modify
+        std::iter::from_fn(|| Some(1 + random(self.sides)))
+            .take(self.count as usize)
+            .sum::<u16>() as i16
+            + self.modify
     }
 }
 
-impl fmt::Display for DieRoll {
+impl fmt::Display for DiceRoll {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if self.modify > 0 {
-            write!(f, "{}d{}+{}", self.count, self.sides, self.modify)
-        } else if self.modify < 0 {
-            write!(f, "{}d{}{}", self.count, self.sides, self.modify)
+        if self.modify != 0 {
+            write!(f, "{}d{}{:+}", self.count, self.sides, self.modify)
         } else {
             write!(f, "{}d{}", self.count, self.sides)
         }
@@ -102,16 +84,16 @@ impl fmt::Display for DieRoll {
 }
 
 fn main() {
-    let args : Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("{}", USAGE_STRING)
-    }
-    match DieRoll::from(&args[1]) {
+    let [_, dice_str] = env::args().collect::<Vec<_>>().leak() else {
+        return println!("{}", USAGE_STRING);
+    };
+    match DiceRoll::from(dice_str) {
         Ok(die) => {
-            println!("{} : {}", die, die.roll());
+            let roll = die.roll();
+            println!("{die} : {roll}");
         }
         Err(e) => {
-            println!("{}", e);
+            println!("{e}");
         }
     }
 }
